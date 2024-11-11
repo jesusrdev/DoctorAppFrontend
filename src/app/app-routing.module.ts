f44c982ce53777055e9ecfc7cf107fcf04f4f8a8
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-11-11 18:32:35 -0500

    adding the form and signIn method in login component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { LoginComponent } from './user/login/login.component';
+import { LayoutComponent } from './shared/layout/layout.component';
 
 const routes: Routes = [
   {
@@ -12,6 +13,11 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
+    pathMatch: 'full',
+  },
+  {
+    path: 'layout',
+    component: LayoutComponent,
     pathMatch: 'full',
   },
   {
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,10 +1,55 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { UserService } from '../services/user.service';
+import { SharedService } from '../../shared/shared.service';
+import { Login } from '../interfaces/login';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  formLogin: FormGroup;
 
+  hidePassword: boolean = true;
+
+  showLoading: boolean = false;
+
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private userService: UserService,
+    private sharedService: SharedService
+  ) {
+    this.formLogin = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  signIn(): void {
+    this.showLoading = true;
+    const request: Login = {
+      username: this.formLogin.value.username,
+      password: this.formLogin.value.password,
+    };
+
+    this.userService.signIn(request).subscribe({
+      next: (response) => {
+        this.sharedService.setSession(response);
+        this.router.navigate(['layout']);
+      },
+      complete: () => {
+        this.showLoading = false;
+      },
+      error: (error) => {
+        this.sharedService.showAlert(error.error, 'Error!');
+        this.showLoading = false;
+      },
+    });
+  }
 }
