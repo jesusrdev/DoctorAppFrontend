41f00d4812caca574a4189750523264d6a6a9b19
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-11-11 18:01:47 -0500

    adding components to shared and user modules

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,16 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms'
 import { HttpClientModule } from '@angular/common/http';
 
+import { LayoutComponent } from './layout/layout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LayoutComponent,
+    DashboardComponent
+  ],
   imports: [
     CommonModule,
   ],
   exports: [
-    ReactiveFormsModule,
     FormsModule,
     HttpClientModule,
+    ReactiveFormsModule,
+
+    DashboardComponent,
+    LayoutComponent,
   ]
 })
 export class SharedModule { }
--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,15 +1,24 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MaterialModule } from '../material/material.module';
+import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 
 import { UserService } from './services/user.service';
 
+import { LoginComponent } from './login/login.component';
+
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    MaterialModule,
+  ],
+  exports: [
+    LoginComponent,
   ],
   providers: [
     UserService,
