7eecb11f505ea6ed3ed36cda1194e110a797679f
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-11-10 22:20:41 -0500

    using http request in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title: string = 'Bienvenido a DoctorApp';
+
+  users: any; // ! Cambiar
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5016/api/User').subscribe({
+      next: response => this.users = response,
+      error: error => console.log(error),
+      complete: () => console.log('Request Complete')
+    });
+  }
+
 }
