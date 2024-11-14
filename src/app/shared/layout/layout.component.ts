01620a1b6a49ebd79a4130f229030af5e9f267f5
jesusrdev <102635058+jesusrdev@users.noreply.github.com>
2024-11-14 11:47:24 -0500

    adding the logout function and show the name of user logged

--- src/app/shared/layout/layout.component.ts
+++ src/app/shared/layout/layout.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styleUrl: './layout.component.css'
+  styleUrl: './layout.component.css',
 })
-export class LayoutComponent {
+export class LayoutComponent implements OnInit {
+  username: string = '';
 
+  constructor(private router: Router, private sharedService: SharedService) {}
+
+  ngOnInit(): void {
+    const userToken = this.sharedService.getSession();
+
+    if (userToken != null) {
+      this.username = userToken.username;
+    }
+  }
+
+  logOut(): void {
+    this.sharedService.deleteSession();
+    this.router.navigate(['login']);
+  }
 }
